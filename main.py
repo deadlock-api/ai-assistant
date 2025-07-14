cfb7f37d127b1e6bc6bbc7468aac6ec96c7bb6a3
Manuel Raimann <raimannma@outlook.de>
2025-07-14 11:29:23 +0200

    Add sqlglot dependency and integrate SQL transpilation in Clickhouse Tool

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import warnings
 
 import requests
+import sqlglot
 from smolagents import CodeAgent, Tool, InferenceClientModel
 
 HF_TOKEN = os.environ.get("HF_TOKEN")
@@ -9,19 +10,20 @@ if HF_TOKEN is None:
     warnings.warn("Provide a HF_TOKEN as environment variable")
     exit(1)
 
+
 class ClickhouseDB(Tool):
     name = "clickhouse_sql"
-    description = "Query the clickhouse DB filled with data about deadlock, with a sql query."
+    description = (
+        "Query the clickhouse DB filled with data about deadlock, with a sql query."
+    )
     inputs = {"sql": {"type": "string", "description": "The sql query to perform."}}
     output_type = "object"
 
     def forward(self, sql: str) -> list[dict]:
-        try:
-            results = requests.get(
-                "https://api.deadlock-api.com/v1/sql", params={"query": sql}
-            ).json()
-        except requests.exceptions.RequestException as e:
-            raise e
+        sql = sqlglot.transpile(sql, write="clickhouse")[0]
+        results = requests.get(
+            "https://api.deadlock-api.com/v1/sql", params={"query": sql}
+        ).json()
         if len(results) == 0:
             raise Exception("No results found!")
         return results
